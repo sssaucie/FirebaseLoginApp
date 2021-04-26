e8ff0da23457eafb403c744c68bc7e19ff60fecc
Emily Thorne <53380331+sssaucie@users.noreply.github.com>
2021-04-25 20:53:31 -0600

    Add login code
    Add logic for sign-in, bug fixes, added Kotlin Gradle config for JVM compatibility

--- app/src/main/java/com/example/android/firebaseui_login_sample/MainFragment.kt
+++ app/src/main/java/com/example/android/firebaseui_login_sample/MainFragment.kt
@@ -60,16 +60,29 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         observeAuthenticationState()
 
-        binding.authButton.setOnClickListener {
-            // TODO call launchSignInFlow when authButton is clicked
-        }
+        binding.authButton.setOnClickListener { launchSignInFlow() }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        // TODO Listen to the result of the sign in process by filter for when
-        //  SIGN_IN_REQUEST_CODE is passed back. Start by having log statements to know
-        //  whether the user has signed in successfully
+        if (requestCode == SIGN_IN_RESULT_CODE)
+        {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK)
+            {
+                // User successfully signed in.
+                Log.i(TAG, "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!")
+            }
+            else
+            {
+                /**
+                 * If sign-in failed. If response is null, the user canceled the sign-in flow
+                 * using the back button. Otherwise, check response.getError().getErrorCode()
+                 * and handle the error.
+                 */
+                Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+            }
+        }
     }
 
     /**
@@ -103,8 +116,30 @@ class MainFragment : Fragment() {
         )
     }
 
-    private fun launchSignInFlow() {
-        // TODO Complete this function by allowing users to register and sign in with
-        //  either their email address or Google account.
+    private fun launchSignInFlow()
+    {
+        /**
+         * Give users the option to sign-in/register with their email or
+         * Google account. If users choose to register with their email,
+         * they will need to create a password as well.
+         */
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+            // This is where you can provide more ways for users to register
+            // and sign in.
+            )
+        /**
+         * Create and launch sign-in intent.
+         * We listen to the response of this activity with the
+         * SIGN_IN_REQUEST_CODE.
+         */
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            MainFragment.SIGN_IN_RESULT_CODE
+        )
     }
 }
