53ccd152955b8a0ab7d3650f3bc903822fb6275f
Emily Thorne <53380331+sssaucie@users.noreply.github.com>
2021-04-25 21:12:20 -0600

    Binding and Live Data
    Added observer and binding for login/logout and supporting logic, added an AuthStateListener and AuthenticationState logic for LiveData and ViewModel files.

--- app/src/main/java/com/example/android/firebaseui_login_sample/LoginViewModel.kt
+++ app/src/main/java/com/example/android/firebaseui_login_sample/LoginViewModel.kt
@@ -44,9 +44,19 @@ class LoginViewModel : ViewModel() {
         AUTHENTICATED, UNAUTHENTICATED, INVALID_AUTHENTICATION
     }
 
-    // TODO Create an authenticationState variable based off the FirebaseUserLiveData object. By
-    //  creating this variable, other classes will be able to query for whether the user is logged
-    //  in or not
+    // Create an authenticationState variable based off the FirebaseUserLiveData object. By
+    // creating this variable, other classes will be able to query for whether the user is logged
+    // in or not
+    val authenticationState = FirebaseUserLiveData().map { firebaseUser ->
+        if (firebaseUser != null)
+        {
+            AuthenticationState.AUTHENTICATED
+        }
+        else
+        {
+            AuthenticationState.UNAUTHENTICATED
+        }
+    }
 
     /**
      * Gets a fact to display based on the user's set preference of which type of fact they want
--- app/src/main/java/com/example/android/firebaseui_login_sample/MainFragment.kt
+++ app/src/main/java/com/example/android/firebaseui_login_sample/MainFragment.kt
@@ -90,19 +90,38 @@ class MainFragment : Fragment() {
      * If there is a logged in user: (1) show a logout button and (2) display their name.
      * If there is no logged in user: show a login button
      */
-    private fun observeAuthenticationState() {
+    private fun observeAuthenticationState()
+    {
         val factToDisplay = viewModel.getFactToDisplay(requireContext())
 
-        // TODO Use the authenticationState variable from LoginViewModel to update the UI
-        //  accordingly.
-        //
-        //  TODO If there is a logged-in user, authButton should display Logout. If the
-        //   user is logged in, you can customize the welcome message by utilizing
-        //   getFactWithPersonalition(). I
-
-        // TODO If there is no logged in user, authButton should display Login and launch the sign
-        //  in screen when clicked. There should also be no personalization of the message
-        //  displayed.
+        // Use the authenticationState variable from LoginViewModel to update the UI
+        // accordingly.
+        viewModel.authenticationState.observe(viewLifecycleOwner, Observer { authenticationState ->
+            when (authenticationState)
+            {
+                // If there is a logged-in user, authButton should display Logout. If the
+                // user is logged in, you can customize the welcome message by utilizing
+                // getFactWithPersonalition().
+                LoginViewModel.AuthenticationState.AUTHENTICATED -> {
+                    binding.welcomeText.text = getFactWithPersonalization(factToDisplay)
+                    binding.authButton.text = getString(R.string.logout_button_text)
+                    binding.authButton.setOnClickListener {
+                        // Implement logging out user
+                        AuthUI.getInstance().signOut(requireContext())
+                    }
+                }
+                else -> {
+                    // If there is no logged in user, authButton should display Login and launch the sign
+                    // in screen when clicked. There should also be no personalization of the message
+                    // displayed.
+                    binding.welcomeText.text = factToDisplay
+                    binding.authButton.text = getString(R.string.login_button_text)
+                    binding.authButton.setOnClickListener {
+                        launchSignInFlow()
+                    }
+                }
+            }
+        })
     }
 
 
